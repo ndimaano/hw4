60e3633457e980e03d5a7b3e30f693cdfd27e62b
Nicolas Dimaano <ndimaano@usc.edu>
2023-03-29 07:26:39 +0000

    completed avl tree implementation

--- avlbst.h
+++ avlbst.h
@@ -137,6 +137,10 @@ protected:
     virtual void nodeSwap( AVLNode<Key,Value>* n1, AVLNode<Key,Value>* n2);
 
     // Add helper functions here
+    void insertFix(AVLNode<Key,Value>* oya, AVLNode<Key,Value>* kodomo);
+		void removeFix(AVLNode<Key, Value>* nood, int nenDiff);
+		void rotateLeft(AVLNode<Key,Value>* kore);
+		void rotateRight(AVLNode<Key,Value>* kore); 
 
 
 };
@@ -149,6 +153,61 @@ template<class Key, class Value>
 void AVLTree<Key, Value>::insert (const std::pair<const Key, Value> &new_item)
 {
     // TODO
+    AVLNode<Key, Value>* pog = new AVLNode<Key, Value>(new_item.first , new_item.second, nullptr);
+		AVLNode<Key, Value>* temp = static_cast<AVLNode<Key,Value>*>(this->root_);
+		if(this->root_==nullptr) {
+			this->root_ = pog;
+		}
+		else {
+			AVLNode<Key, Value>* champ = static_cast<AVLNode<Key,Value>*>(this->internalFind(new_item.first));
+			if(champ!=nullptr) {
+				delete pog;
+				champ->setValue(new_item.second);
+				return;
+			}
+			while(temp != nullptr) {
+				if(new_item.first < temp->getKey()) {
+					if(temp->getLeft() == nullptr) {
+						pog->setParent(temp);
+						temp->setLeft(pog);
+						if(pog->getParent()->getBalance()==-1 || pog->getParent()->getBalance()==1) {
+							pog->getParent()->setBalance(0);
+						}
+						else if(pog->getParent()->getBalance()==0) {
+							if(pog==pog->getParent()->getLeft()) {
+								pog->getParent()->setBalance(-1);
+							}
+							else {
+								pog->getParent()->setBalance(1);
+							}
+							insertFix(pog->getParent(), pog);
+						}
+						return;
+					}
+					temp = temp->getLeft();
+				}
+				if(new_item.first > temp->getKey()) {
+					if(temp->getRight() == nullptr) {
+						pog->setParent(temp);
+						temp->setRight(pog);
+						if(pog->getParent()->getBalance()==-1 || pog->getParent()->getBalance()==1) {
+							pog->getParent()->setBalance(0);
+						}
+						else if(pog->getParent()->getBalance()==0) {
+							if(pog==pog->getParent()->getLeft()) {
+								pog->getParent()->setBalance(-1);
+							}
+							else {
+								pog->getParent()->setBalance(1);
+							}
+							insertFix(pog->getParent(), pog);
+						}
+						return;
+					}
+					temp = temp->getRight();
+				}
+			} 
+		}
 }
 
 /*
@@ -159,7 +218,497 @@ template<class Key, class Value>
 void AVLTree<Key, Value>:: remove(const Key& key)
 {
     // TODO
+    AVLNode<Key, Value> * arimasen = static_cast<AVLNode<Key,Value>*>(this->internalFind(key));
+		if(arimasen != nullptr) {
+			if (arimasen->getLeft() != nullptr && arimasen->getRight()!=nullptr){
+				nodeSwap(arimasen, static_cast<AVLNode<Key,Value>*>(BinarySearchTree<Key,Value>::predecessor(arimasen)));
+			}
+			int diff= 0;
+			AVLNode<Key, Value>* poppy = arimasen->getParent();
+			if(arimasen->getParent() != nullptr) {
+				if(arimasen->getParent()->getLeft()==arimasen) {
+					diff = 1;
+				}
+				else if (arimasen->getParent()->getRight()==arimasen) {
+					diff = -1;
+				}
+			}
+			if (arimasen->getLeft() == nullptr && arimasen->getRight()==nullptr) {
+				if(arimasen->getParent()==nullptr) {
+					this->root_=nullptr;
+				}
+				else if (arimasen == arimasen->getParent()->getLeft()) {
+					arimasen->getParent()->setLeft(nullptr) ;
+				}
+				else if(arimasen == arimasen->getParent()->getRight()) {
+					arimasen->getParent()->setRight(nullptr);
+				}
+				delete arimasen;
+			}
+			else if (arimasen->getLeft() != nullptr && arimasen->getRight()==nullptr) {
+				if(arimasen->getParent()==nullptr) {
+					arimasen->getLeft()->setParent(nullptr);
+					this->root_=arimasen->getLeft();
+				}
+				else if(arimasen == arimasen->getParent()->getLeft()) {
+					arimasen->getParent()->setLeft(arimasen->getLeft()) ;
+					arimasen->getLeft()->setParent(arimasen->getParent());
+				}
+				else if(arimasen == arimasen->getParent()->getRight()) {
+					arimasen->getParent()->setRight(arimasen->getLeft());
+					arimasen->getLeft()->setParent(arimasen->getParent());
+				}
+				delete arimasen;
+			}
+			else if (arimasen->getLeft() == nullptr && arimasen->getRight()!=nullptr) {
+				if(arimasen->getParent()==nullptr) {
+					arimasen->getRight()->setParent(nullptr);
+					this->root_=arimasen->getRight();
+				}
+				else if(arimasen == arimasen->getParent()->getLeft()) {
+					arimasen->getParent()->setLeft(arimasen->getRight());
+					arimasen->getRight()->setParent(arimasen->getParent());
+				}
+				else if(arimasen == arimasen->getParent()->getRight()) {
+					arimasen->getParent()->setRight(arimasen->getRight());
+					arimasen->getRight()->setParent(arimasen->getParent());
+				}
+				delete arimasen;
+			}
+			removeFix(poppy, diff); 
+		}
 }
+
+// template<class Key, class Value>
+// bool AVLTree<Key,Value>::hasChildren(AVLNode<Key,Value>* kore) {
+// 	if(kore->getRight() != nullptr || kore->getLeft()!=nullptr){
+// 		return true;
+// 	}
+// 	return false
+// }
+
+template<class Key, class Value>
+void AVLTree<Key,Value>::rotateRight(AVLNode<Key,Value>* z) {
+	// AVLNode<Key,Value>* Parent = kore->getParent();
+	// AVLNode<Key,Value>* leftChild = kore->getLeft();
+
+	// kore->setLeft(leftChild->getRight());
+
+	// if(leftChild->getRight() != nullptr) {
+	// 	leftChild->getRight()->setParent(kore);
+	// }
+
+	// leftChild->setRight(kore);
+	// kore->setParent(leftChild);
+	// leftChild->setParent(Parent);
+
+	// if(Parent != nullptr) {
+	// 	if(kore == Parent->getLeft()) {
+	// 		Parent->setLeft(leftChild);
+	// 	}
+	// 	if(kore == Parent->getRight()) {
+	// 		Parent->setRight(leftChild);
+	// 	}
+	// }
+
+	if(z->getParent()!=nullptr) {
+		if(z==z->getParent()->getLeft()) {
+			z->getParent()->setLeft(z->getLeft());
+		}
+		else if(z==z->getParent()->getRight()) {
+			z->getParent()->setRight(z->getLeft());
+		}
+		if (z->getLeft()!=nullptr) {
+			z->getLeft()->setParent(z->getParent());
+		}
+		z->setParent(z->getLeft());
+
+
+
+		if(z->getParent()!=nullptr) {
+			z->setLeft(z->getParent()->getRight());
+		}
+		else {
+			z->setLeft(nullptr);
+		}
+
+
+
+
+
+
+		if(z->getLeft()!= nullptr) {
+			z->getLeft()->setParent(z);
+		}
+		if(z->getRight()!=nullptr) {
+			z->getRight()->setParent(z);
+		}
+
+
+
+
+		if(z->getParent()!=nullptr) {
+			z->getParent()->setRight(z);
+		}
+
+
+
+
+		return;
+	}
+	if (z->getLeft()!=nullptr) {
+		z->getLeft()->setParent(z->getParent());
+	}
+	z->setParent(z->getLeft());
+	if(z->getParent()!=nullptr) {
+		z->setLeft(z->getParent()->getRight());
+	}
+	else {
+		z->setLeft(nullptr);
+	}
+	if(z->getLeft()!= nullptr) {
+		z->getLeft()->setParent(z);
+	}
+	if(z->getRight()!=nullptr) {
+		z->getRight()->setParent(z);
+	}
+	if(z->getParent()!=nullptr) {
+			z->getParent()->setRight(z);
+	}
+	this->root_ = z->getParent();
+}
+
+template<class Key, class Value>
+void AVLTree<Key,Value>::rotateLeft(AVLNode<Key,Value>* z) {
+	// if(z->getParent()!=nullptr) {
+		// 	if(z==z->getParent()->getLeft()) {
+		// 		z->getParent()->setLeft(z->getRight());
+		// 	}
+		// 	else if(z==z->getParent()->getRight()) {
+		// 		z->getParent()->setRight(z->getRight());
+		// 	}
+		// 	z->getRight()->setParent(z->getParent());
+		// 	z->setParent(z->getRight());
+		// 	z->setRight(z->getParent()->getLeft());
+		// 	if(z->getLeft()!=nullptr) {
+		// 		z->getLeft()->setParent(z);
+		// 	}
+		// 	z->getLeft()->setParent(z);
+		// 	z->getParent()->setLeft(z);
+		// }
+		// z->getRight()->setParent(z->getParent());
+		// z->setParent(z->getRight());
+		// z->setRight(z->getParent()->getLeft());
+		// if(z->getLeft()!=nullptr) {
+		// 	z->getLeft()->setParent(z);
+		// }
+		// z->getLeft()->setParent(z);
+		// z->getParent()->setLeft(z);
+		// this->root_ = z->getParent();
+		if(z->getParent()!=nullptr) {
+			if(z==z->getParent()->getLeft()) {
+				z->getParent()->setLeft(z->getRight());
+			}
+			else if(z==z->getParent()->getRight()) {
+				z->getParent()->setRight(z->getRight());
+			}
+			if (z->getRight()!=nullptr) {
+				z->getRight()->setParent(z->getParent());
+			}
+			z->setParent(z->getRight());
+			if(z->getParent()!=nullptr) {
+				z->setRight(z->getParent()->getLeft());
+			}
+			else {
+				z->setRight(nullptr);
+			}
+			if(z->getRight()!=nullptr) {
+				z->getRight()->setParent(z);
+			}
+			if(z->getLeft()!=nullptr) {
+				z->getLeft()->setParent(z);
+			}
+			if(z->getParent()!=nullptr) {
+				z->getParent()->setLeft(z);
+			}
+			return;
+		}
+		if (z->getRight()!=nullptr) {
+			z->getRight()->setParent(z->getParent());
+		}
+		z->setParent(z->getRight());
+
+
+
+
+		if(z->getParent()!=nullptr) {
+			z->setRight(z->getParent()->getLeft());
+		}
+		else {
+			z->setRight(nullptr);
+		}
+
+
+
+
+
+
+		if(z->getRight()!=nullptr) {
+			z->getRight()->setParent(z);
+		}
+		if(z->getLeft()!=nullptr) {
+			z->getLeft()->setParent(z);
+		}
+
+		if(z->getParent()!=nullptr) {
+			z->getParent()->setLeft(z);
+		}
+
+
+		this->root_ = z->getParent();
+	}
+
+
+
+	// AVLNode<Key,Value>* Parent = kore->getParent();
+	// AVLNode<Key,Value>* rightChild = kore->getRight();
+
+	// kore->setRight(rightChild->getLeft());
+
+	// if(rightChild->getLeft() != nullptr) {
+	// 	rightChild->getLeft()->setParent(kore);
+	// }
+
+	// rightChild->setLeft(kore);
+	// kore->setParent(rightChild);
+	// rightChild->setParent(Parent);
+
+	// if(Parent != nullptr) {
+	// 	if(kore == Parent->getLeft()) {
+	// 		Parent->setLeft(rightChild);
+	// 	}
+	// 	if(kore == Parent->getRight()) {
+	// 		Parent->setRight(rightChild);
+	// 	}
+	// }
+
+
+
+template<class Key, class Value>
+void AVLTree<Key,Value>::insertFix(AVLNode<Key,Value>* oya, AVLNode<Key,Value>* kodomo) {
+	if(oya == nullptr || oya->getParent() == nullptr) {
+		return;
+	}
+	AVLNode<Key,Value>*otou = oya->getParent();
+	if(oya==otou->getLeft()) {
+		otou->updateBalance(-1);
+		if(otou->getBalance() == 0) {
+			return;
+		}
+		else if(otou->getBalance()==-1) {
+			insertFix(otou, oya);
+		}
+		else if(otou->getBalance()==-2) {
+			if(oya->getLeft()==kodomo) {
+				rotateRight(otou);
+				oya->setBalance(0);
+				otou->setBalance(0);
+			}
+			else {
+				rotateLeft(oya);
+				rotateRight(otou);
+				if(kodomo->getBalance()==-1) {
+					oya->setBalance(0);
+					otou->setBalance(1);
+					kodomo->setBalance(0);
+				}
+				else if(kodomo->getBalance()==0) {
+					oya->setBalance(0);
+					otou->setBalance(0);
+					kodomo->setBalance(0);
+				}
+				else if(kodomo->getBalance()==1) {
+					oya->setBalance(-1);
+					otou->setBalance(0);
+					kodomo->setBalance(0);
+				}
+			}
+		}
+	}
+	else if(oya==otou->getRight()) {
+		otou->updateBalance(1);
+		if(otou->getBalance() == 0) {
+			return;
+		}
+		else if(otou->getBalance()==1) {
+			insertFix(otou, oya);
+		}
+		else if(otou->getBalance()==2) {
+			if(oya->getRight()==kodomo) {
+				rotateLeft(otou);
+				oya->setBalance(0);
+				otou->setBalance(0);
+			}
+			else {
+				rotateRight(oya);
+				rotateLeft(otou);
+				if(kodomo->getBalance()==-1) {
+					oya->setBalance(1);
+					otou->setBalance(0);
+					kodomo->setBalance(0);
+				}
+				else if(kodomo->getBalance()==0) {
+					oya->setBalance(0);
+					otou->setBalance(0);
+					kodomo->setBalance(0);
+				}
+				else if(kodomo->getBalance()==1) {
+					oya->setBalance(0);
+					otou->setBalance(-1);
+					kodomo->setBalance(0);
+				}
+			}
+		}
+	}
+}
+
+template<class Key, class Value>
+void AVLTree<Key,Value>::removeFix(AVLNode<Key, Value>* nood, int nenDiff) {
+	if(nood == nullptr) {
+		return;
+	}
+	AVLNode<Key, Value>* oya= nood->getParent();
+	int diff = 0;
+	if(oya!=nullptr) {
+		if(nood == oya->getLeft()) {
+			diff = 1;
+		}
+		else if(nood==oya->getRight()) {
+			diff = -1;
+		}
+	}
+	// else if(nood->getLeft()!=nullptr && nood->getRight()!=nullptr) {
+	// 	if(nood->getLeft()->getKey() > nood->getRight()->getKey()) {
+	// 		c = nood->getLeft();
+	// 	}
+	// 	else if (nood->getLeft()->getKey() < nood->getRight()->getKey()) {
+	// 		c = nood->getRight();
+	// 	}
+	// }
+	// else if(nood->getLeft()==nullptr) {
+	// 	c = nood->getRight();
+	// }
+	// else if(nood->getRight()==nullptr) {
+	// 	c = nood->getLeft();
+	// }
+
+	if(nenDiff==-1) {
+		if(nood->getBalance() + nenDiff == -2) {
+			AVLNode<Key, Value>* c = nood->getLeft();
+			if(c->getBalance() == -1) {
+				rotateRight(nood);
+				nood->setBalance(0);
+				c->setBalance(0);
+				removeFix(oya, diff);
+			}
+			else if(c->getBalance()==0) {
+				rotateRight(nood);
+				nood->setBalance(-1);
+				c->setBalance(1);
+			}
+			else if(c->getBalance()==1) {
+				AVLNode<Key, Value>* g = c->getRight();
+				rotateLeft(c);
+				rotateRight(nood);
+				if(g->getBalance()==1) {
+					nood->setBalance(0);
+					c->setBalance(-1);
+					g->setBalance(0);
+				}
+				else if(g->getBalance()==0) {
+					nood->setBalance(0);
+					c->setBalance(0);
+					g->setBalance(0);
+				}
+				else if(g->getBalance()==-1) {
+					nood->setBalance(1);
+					c->setBalance(0);
+					g->setBalance(0);
+				}
+				removeFix(oya, diff);
+			}
+		}
+		else if (nood->getBalance() + nenDiff == -1) {
+			nood->setBalance(-1);
+		}
+		else if (nood->getBalance() + nenDiff == 0) {
+			nood->setBalance(0);
+			removeFix(oya, diff);
+		}
+		
+	}
+	else {
+		if(nood->getBalance() + nenDiff == 2) {
+		AVLNode<Key, Value>* c = nood->getRight();
+		if(c->getBalance() == 1) {
+			rotateLeft(nood);
+			nood->setBalance(0);
+			c->setBalance(0);
+			removeFix(oya, diff);
+		}
+		else if(c->getBalance()==0) {
+			rotateLeft(nood);
+			nood->setBalance(1);
+			c->setBalance(-1);
+		}
+		else if(c->getBalance()==-1) {
+			AVLNode<Key, Value>* g = c->getLeft();
+			rotateRight(c);
+			rotateLeft(nood);
+			if(g->getBalance()==1) {
+				nood->setBalance(-1);
+				c->setBalance(0);
+				g->setBalance(0);
+			}
+			else if(g->getBalance()==0) {
+				nood->setBalance(0);
+				c->setBalance(0);
+				g->setBalance(0);
+			}
+			else if(g->getBalance()==-1) {
+				nood->setBalance(0);
+				c->setBalance(1);
+				g->setBalance(0);
+			}
+			removeFix(oya,diff);
+		}
+
+	}
+	else if (nood->getBalance() + nenDiff == 1) {
+		nood->setBalance(1);
+	}
+	else if (nood->getBalance() + nenDiff == 0) {
+		nood->setBalance(0);
+		removeFix(oya, diff);
+	}
+}
+}
+
+// void AVLTree<Key, Value>::baransuChecker(AVLNode<Key, Value>* kore) {
+// 	if(abs(kore->getParent()->getBalance()) == 1) {
+// 		return;
+// 	}
+// 	else if (kore->getParent()->getBalance()==0){
+// 		if(kore->getParent()->getLeft()==kore) {
+// 			kore->getParent()->setBalance(-1);
+// 		}
+// 		if(kore->getParent()->getRight()==kore) {
+// 			kore->getParent()->setBalance(1);
+// 		}
+// 		insertFix(kore->getParent(), kore);
+// 	}
+// }
+
 
 template<class Key, class Value>
 void AVLTree<Key, Value>::nodeSwap( AVLNode<Key,Value>* n1, AVLNode<Key,Value>* n2)
@@ -169,6 +718,7 @@ void AVLTree<Key, Value>::nodeSwap( AVLNode<Key,Value>* n1, AVLNode<Key,Value>* 
     n1->setBalance(n2->getBalance());
     n2->setBalance(tempB);
 }
+
 
 
 #endif
