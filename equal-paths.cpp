1dbabe3ab5031d7820239ef311d6e66659ad1952
Nicolas Dimaano <ndimaano@usc.edu>
2023-03-29 08:31:08 +0000

    added comments

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,30 +8,30 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-bool help(Node * nood, int& count) {
-    if(nood == nullptr) {
+bool help(Node * nood, int& count) { // made this so that I could actually use a counter
+    if(nood == nullptr) { // handles the case where the node is null
         return true;
     }
-    int migikaunto=0;
-    int hidarikaunto=0;
-    bool hidari = help(nood->left, hidarikaunto);
-    bool migi = help(nood->right, migikaunto);
-    if (hidari && migi) {
-        if(migikaunto==hidarikaunto || nood->left == nullptr || nood->right == nullptr) {
+    int migikaunto=0; // counter for the right tree
+    int hidarikaunto=0; // counter for the left tree
+    bool hidari = help(nood->left, hidarikaunto); // recursive call for the left tree
+    bool migi = help(nood->right, migikaunto); // recursive call for the right tree
+    if (hidari && migi) { // only runs if both left and right subtrees are equal because it will not be equal if they are not
+        if(migikaunto==hidarikaunto || nood->left == nullptr || nood->right == nullptr) { //this checks the height of the two subtrees and compares them
             if(hidarikaunto >= migikaunto) {
-                count = hidarikaunto + 1;
+                count = hidarikaunto + 1; // increases the left count by 1 to include the node being checked
                 return true;
             }
             else {
-                count = migikaunto + 1;
+                count = migikaunto + 1; //increases the right count by 1 to include the node being checked
                 return true;
             }
         }
-        else {
+        else { // handles the case where the subtrees do not have the same height and are not null
             return false;
         }
     } 
-    else {
+    else { // handles the ase where the subtrees are not equal
         return false;
     }
     return false;
@@ -41,6 +41,6 @@ bool equalPaths(Node * root)
 {
     // Add your code below
     int count =0;
-    return help(root, count);
+    return help(root, count); // call to another recursive function that tracks the count which is the height
 }
 
